a359dded1800001f9006fa0a8e0685786823504a
Mona <mona8xoe11@gmail.com>
2025-12-04 16:05:27 +0300

    تم تحديث الملف final.py وإجراء التعديلات المطلوبة

--- final.py
+++ final.py
@@ -1,26 +1,14 @@
-<div id="carouselExampleCaptions" class="carousel slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
+products = [
+    {"name":  "طلاء اظافر", "price": 10},
+    {"name": "فرشاة شعر", "price": 20},
+    {"name": "سماعة بلوتوث", "price": 500},
+    {"name": "ماكينة تحضير القهوة", "price": 299},
+    {"name": "تمثال حوت الاوركا ", "price": 350}
+]
 
-  <!-- الصور داخل الـ carousel -->
-  <div class="carousel-item active">
-    <img src="https://i.pinimg.com/736x/e4/42/f1/e442f1372f45d7a2f66f85b07dcf588b.jpg" class="d-block w-100" alt="...">
-    <div class="carousel-caption d-none d-md-block">
-      <h5>حرية الفراشة</h5>
-      <p>لوحة تعبر عن الحرية بشكل مبهر وجميل</p>
-    </div>
-  </div>
+tax_rate = 0.10
 
-  <!-- أزرار التنقل بين الصور -->
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+for product in products:
+    price_with_tax = product["price"] * (1 + tax_rate)
+    print(f"السعر النهائي لـ {product['name']} بعد الضريبة هو: {price_with_tax:.2f} ريال")
+
