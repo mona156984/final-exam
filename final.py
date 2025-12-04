aaacf051b945da5876f687f0296600acaeb2f5af
Mona <mona8xoe11@gmail.com>
2025-12-04 04:14:27 +0300

    تحديث تصاميم الصفحات وتحسين التنسيق

---
+++ final.py
@@ -0,0 +1,26 @@
+<div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+
+  <!-- الصور داخل الـ carousel -->
+  <div class="carousel-item active">
+    <img src="https://i.pinimg.com/736x/e4/42/f1/e442f1372f45d7a2f66f85b07dcf588b.jpg" class="d-block w-100" alt="...">
+    <div class="carousel-caption d-none d-md-block">
+      <h5>حرية الفراشة</h5>
+      <p>لوحة تعبر عن الحرية بشكل مبهر وجميل</p>
+    </div>
+  </div>
+
+  <!-- أزرار التنقل بين الصور -->
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
